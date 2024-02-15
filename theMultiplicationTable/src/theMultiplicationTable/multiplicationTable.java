c42e9998bd82fc533b1da548f59c34332a3066f5
Koray <150171299+koraytacus@users.noreply.github.com>
2024-02-15 19:35:29 +0300

    Add files via upload

---
+++ theMultiplicationTable/src/theMultiplicationTable/multiplicationTable.java
@@ -0,0 +1,39 @@
+package theMultiplicationTable;
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+		
+		
+		int startNumb = 1;
+		while (startNumb<=10) {
+			
+			
+			for(int a = 1;a<=10;a++) {
+				
+				System.out.println(startNumb + " x " + a + " = " + startNumb*a);
+				
+			}
+			
+				startNumb++;
+				System.out.println("---------------------------------------------------------------------------");
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
