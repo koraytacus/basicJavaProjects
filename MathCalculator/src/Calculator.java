6101e6c67e0e99ec4b8c942041c796b739158560
Koray <150171299+koraytacus@users.noreply.github.com>
2024-02-18 12:06:42 +0300

    Update Calculator.java
    I added some explanation

--- MathCalculator/src/Calculator.java
+++ MathCalculator/src/Calculator.java
@@ -1,31 +1,35 @@
 import java.util.Scanner;
 
 public class Calculator {
-		
-	//We create a method for displaying our calculation
-	static void showing(int value1,String operation,int value2, int result) {
-		
+
+	// We create a method for displaying our calculation
+	static void showing(int value1, String operation, int value2, int result) {
+
 		System.out.println(value1 + " " + operation + " " + value2 + " = " + result);
 	}
-	
+
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in); // We create a Scanner Object
 		System.out.println("The App is uploading...");
+		System.out.println(
+				" 1) + -> addition" + " 2) - -> subtraction" + " 3) * -> multiplication" + " 4) / -> division" + " 5) % -> modulus"
+						+ " 6) = -> this sign gives main result of calculation ");
 		System.out.println("You can start the calculation :");
 
 		int result = 0;
-		
+
 		System.out.println("number :");
 		int firstNumber = input.nextInt(); // We take a first number
-		input.nextLine();//This is used to consume the newline character left in the input after reading the value
+		input.nextLine();// This is used to consume the newline character left in the input after reading
+							// the value
 
 		while (true) {
 			System.out.println("operation :");
 			String sign = input.nextLine();// We take a first operation sign
 
 			if (sign.equals("=")) {
-				System.out.println("Sonuç: " + firstNumber); //We determined finished condition for while loop
+				System.out.println("Sonuç: " + firstNumber); // We determined finished condition for while loop
 				break;
 			}
 
@@ -36,26 +40,26 @@ public class Calculator {
 
 			case "+":
 				result = firstNumber + otherNumber;
-				showing(firstNumber,sign,otherNumber,result);
+				showing(firstNumber, sign, otherNumber, result);
 				firstNumber = result;
 				break;
 
 			case "-":
 				result = firstNumber - otherNumber;
-				showing(firstNumber,sign,otherNumber,result);
+				showing(firstNumber, sign, otherNumber, result);
 				firstNumber = result;
 				break;
 
 			case "*":
 				result = firstNumber * otherNumber;
-				showing(firstNumber,sign,otherNumber,result);
+				showing(firstNumber, sign, otherNumber, result);
 				firstNumber = result;
 				break;
 
 			case "/":
 				if (otherNumber != 0) {
 					result = firstNumber / otherNumber;
-					showing(firstNumber,sign,otherNumber,result);
+					showing(firstNumber, sign, otherNumber, result);
 					firstNumber = result;
 				} else {
 					System.out.println("Zero Division Error,Program is rebooting...");
@@ -67,21 +71,21 @@ public class Calculator {
 			case "%":
 				if (otherNumber != 0) {
 					result = firstNumber % otherNumber;
-					showing(firstNumber,sign,otherNumber,result);
-					firstNumber = result;}
-				else {
+					showing(firstNumber, sign, otherNumber, result);
+					firstNumber = result;
+				} else {
 					System.out.println("Zero Division Error,Program is rebooting...");
 					System.out.println("Number : ");
 					firstNumber = input.nextInt();
 				}
 				break;
 
-				default:
-					System.out.println("Incorrect entry");
-                    break;
+			default:
+				System.out.println("Incorrect entry");
+				break;
 			}
 
-			sign = input.nextLine(); //We are taking new operation sign
+			sign = input.nextLine(); // We are taking new operation sign
 		}
 		input.close();
 
