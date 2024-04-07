a282422a0392278e1d3bde537255284b7a51aa47
Koray <150171299+koraytacus@users.noreply.github.com>
2024-04-07 03:31:09 +0300

    Add files via upload

---
+++ Question5/SpeciesTest.java
@@ -0,0 +1,28 @@
+/**
+ * Write a program to answer questions like the following:
+ * Suppose the species Klingon ox has a population of 100 and a growth rate of
+ * 15 percent, and it lives in an area of 1500 square miles. How long would
+ * it take for the population density to exceed 1 per square mile?
+ *
+ * The formula for population density is population divided by area.
+ *
+ * Test your program with the following values:
+ * Population: 100
+ * Growth rate: 15%
+ * Area: 1500 square miles
+ * */
+
+
+public class SpeciesTest {
+
+    public static void main(String[] args) {
+        Species x1 = new Species("Ailurus fulgens",2000,0.3,10000);
+        System.out.println(x1);
+    }
+
+
+
+
+
+
+}
