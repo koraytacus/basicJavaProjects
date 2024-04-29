cb90e4590fe183d5f38a3309606a9bee9448780c
Koray <150171299+koraytacus@users.noreply.github.com>
2024-04-29 21:32:54 +0300

    Add files via upload
    In this exercise , basic shape hierarchy is executed with abstraction

---
+++ AbstractionExercise1Shape/src/Circle.java
@@ -0,0 +1,30 @@
+
+
+public class Circle extends Shape{
+	
+	private double radius;
+
+	public Circle(double radius) {
+		super("Circle");
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI*Math.pow(radius, 2);
+		
+	}
+
+	@Override
+	public double perimeter() {
+		return Math.PI*radius*2;
+		
+	}
+
+	@Override
+	public String toString() {
+		
+		return super.toString();
+
+}
+}
